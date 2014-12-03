b756fccf1762f01b8656189748d4b84af5da1b7f
dkowald <dkowald@know-center.at>
2014-12-03 15:10:18 +0100

    added interface for cf engine

---
+++ src/engine/ResourceEngineInterface.java
@@ -0,0 +1,31 @@
+/*
+ TagRecommender:
+ A framework to implement and evaluate algorithms for the recommendation
+ of tags.
+ Copyright (C) 2013 Dominik Kowald
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+ 
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package engine;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ResourceEngineInterface {
+	
+	public void loadFile(String filename) throws Exception;
+	
+	public Map<String, Double> getEntitiesWithLikelihood(String user, String resource, List<String> topics, Integer count, Boolean filterOwnEntities);
+}
